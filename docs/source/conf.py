69b91db3f5508f82b0d73cbfa3311dc628d0f73b
mza79 <39781646+mza79@users.noreply.github.com>
2022-07-22 00:19:33 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'How to Use Read the Docs Documentation '
+copyright = '2022,Tom Zhang'
+author = 'Tom Zhang'
 
 release = '0.1'
 version = '0.1.0'
