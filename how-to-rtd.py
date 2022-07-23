4ef33045d7e7b5545f3cbb15a7a2e75e64a4e383
Tom <tomzhangmc@gmail.com>
2022-07-23 16:45:09 -0700

    test configs

--- how-to-rtd.py
+++ how-to-rtd.py
@@ -4,8 +4,8 @@ apis containing functions that we all love
 
 __version__ = "0.1.0"
 
-# from datetime import datetime
-# from time import strftime
+from datetime import datetime
+from time import strftime
 
 def get_today():
     """
