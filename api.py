385442b2399530bad40808c7dc01eebcbfaf71cf
Tom <tomzhangmc@gmail.com>
2022-07-23 19:11:59 -0700

    add another function

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ api - containing functions that we all love
 
 from datetime import datetime
 from time import strftime
+import random
 
 __version__ = "0.1.0"
 
@@ -16,4 +17,11 @@ def get_today():
     """
     return datetime.now().strftime('%d%m%Y')
 
+def get_random_number():
+    """
+    Return a random number bewteen 0 - 100
 
+    :return: a random number bewteen 0 - 100
+    :rtype: int
+    """
+    return random.randint(0, 100)
