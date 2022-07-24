e6121809509f6bb54a8639e81e9ea38b17e8a5a2
Tom <tomzhangmc@gmail.com>
2022-07-23 18:32:06 -0700

    edited api page

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
+from datetime import datetime
+from time import strftime
+
 """
 apis containing functions that we all love
 """
 
 __version__ = "0.1.0"
-
-from datetime import datetime
-from time import strftime
 
 def get_today():
     """
